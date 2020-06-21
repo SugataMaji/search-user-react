4ec7dadf4644a63df033a33e0edfdf9e026ca940
Sugata Maji <sugatamaji@Xcelpross-MacBook-Pro.local>
2020-06-21 12:59:23 +0530

    Updates

---
+++ precache-manifest.eb16dd72532bcb030aca5d1a1c6a9c30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58c20e74a3ac6086f417c7e09387279",
+    "url": "/search-user-react/index.html"
+  },
+  {
+    "revision": "ca035f7833bdcf945565",
+    "url": "/search-user-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e206060a09338fa7eee",
+    "url": "/search-user-react/static/css/main.d58fb3eb.chunk.css"
+  },
+  {
+    "revision": "ca035f7833bdcf945565",
+    "url": "/search-user-react/static/js/2.66ed18f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-user-react/static/js/2.66ed18f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e206060a09338fa7eee",
+    "url": "/search-user-react/static/js/main.863c5aa6.chunk.js"
+  },
+  {
+    "revision": "ba398029febb82fd1f74",
+    "url": "/search-user-react/static/js/runtime-main.38c376ed.js"
+  }
+]);
